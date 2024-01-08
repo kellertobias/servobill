4cb5dabb6d46a42f34cd8487e639be3d7a03c380
Tobias S. Keller <t.keller@blink.it>
2024-01-08 19:51:23 +0100

    Date Picker was hidden in add payment

--- src/app/_components/dialog.tsx
+++ src/app/_components/dialog.tsx
@@ -58,8 +58,8 @@ export function Dialog(props: {
 							leaveFrom="opacity-100 translate-y-0 sm:scale-100"
 							leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
 						>
-							<HeadlessDialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-lg">
-								<div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+							<HeadlessDialog.Panel className="relative transform rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-full sm:max-w-lg">
+								<div className="px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
 									<div className="sm:flex sm:items-start">
 										<div
 											className={clsx(
