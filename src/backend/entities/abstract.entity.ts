366586bb83642153fc7ad3231e125d7d112b35cd
Tobias S. Keller <t.keller@blink.it>
2025-06-19 14:56:27 +0200

    fix: avoid sending out events twice

--- src/backend/entities/abstract.entity.ts
+++ src/backend/entities/abstract.entity.ts
@@ -1,5 +1,8 @@
+import { randomUUID } from 'crypto';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export class DomainEvent<T = any> {
+	public readonly id: string;
 	public readonly occurredOn: Date;
 
 	constructor(
@@ -7,18 +10,28 @@ export class DomainEvent<T = any> {
 		public readonly name: string,
 		public readonly data: T,
 	) {
+		this.id = randomUUID();
 		this.occurredOn = new Date();
 	}
 }
 
 export abstract class DomainEntity {
+	private sentEvents = new Set<string>();
 	public abstract id: string;
 	public events: DomainEvent[] = [];
 	protected addEvent(event: DomainEvent) {
+		if (this.sentEvents.has(event.id)) {
+			return;
+		}
+		this.sentEvents.add(event.id);
 		this.events.push(event);
 	}
 	public async purgeEvents(send: (event: DomainEvent) => Promise<void>) {
 		for (const event of this.events) {
+			if (this.sentEvents.has(event.id)) {
+				continue;
+			}
+			this.sentEvents.add(event.id);
 			await send(event);
 		}
 		this.events = [];
